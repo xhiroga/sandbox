21a9935a6cd5fdaa9c60230c0714adc9867dc190
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-01-02 21:03:26 +0900

    テーブルレイアウトの変更を反映(これ自動化したい...）

--- sql/ddl/4.sql
+++ sql/ddl/4.sql
@@ -1,26 +1,22 @@
--- Table: public."PGM_STEP_COUNT"
+-- Table: public.pgm_step_count
 
--- DROP TABLE public."PGM_STEP_COUNT";
+-- DROP TABLE public.pgm_step_count;
 
-CREATE TABLE public."pgm_step_count"
+CREATE TABLE public.pgm_step_count
 (
-  "create_timestamp" timestamp with time zone,
-  "update_timestamp" timestamp with time zone,
-  "delete_timestamp" timestamp with time zone,
-  "id" integer NOT NULL DEFAULT nextval('"pgm_step_count_id_seq"'::regclass),
-  "user_id" integer,
-  "date" date,
-  "step_count" integer,
-  CONSTRAINT "pgm_step_count_pkey" PRIMARY KEY ("id"),
-  CONSTRAINT "pgm_step_count_user_ID_fkey" FOREIGN KEY ("user_id")
-      REFERENCES public."user" ("user_id") MATCH SIMPLE
+  create_timestamp timestamp with time zone NOT NULL,
+  update_timestamp timestamp with time zone NOT NULL,
+  delete_timestamp timestamp with time zone NOT NULL,
+  user_id character varying NOT NULL,
+  date date NOT NULL,
+  count integer NOT NULL,
+  CONSTRAINT pmg_step_count_pkey PRIMARY KEY (user_id, date),
+  CONSTRAINT pmg_step_count_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES public."user" (user_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE public."pgm_step_count"
+ALTER TABLE public.pgm_step_count
   OWNER TO hiroga;
-COMMENT ON TABLE public."pgm_step_count"
-  IS 'ユーザーごとの1日に書いたプログラムのステップ数';
-
