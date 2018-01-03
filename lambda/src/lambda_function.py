a8f4fb8f569bf5b4dc2c5c093ec592dbb10c0276
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-01-03 16:53:29 +0900

    よく考えたら前日のプログラミング量を取らなくちゃいけなかった。修正

--- lambda/src/lambda_function.py
+++ lambda/src/lambda_function.py
@@ -2,16 +2,17 @@
 # lambda_function.lambda_handler
 from datetime import date
 from datetime import datetime as dt
+from datetime import timedelta
 import add_step_count as add
 import postgres as pg
 
 
 def lambda_handler(event, context):
-    day = dt.strftime(date.today(), '%Y-%m-%d')
+    yesterday = dt.strftime(date.today() - timedelta(days=1), '%Y-%m-%d')
     for userid in pg.get_users():
         repos = pg.get_repos(userid)
-        total_count = add.get_addition_count(day, userid, repos)
-        pg.insert_total_count(day, userid, total_count)
+        total_count = add.get_addition_count(yesterday, userid, repos)
+        pg.insert_total_count(yesterday, userid, total_count)
     print("OK")
 
 
